b440ccaa18660f4eb176ea4a3575076c42f32985
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-10-25 16:05:49 -0300

    added the basic code of the dht service

--- DHTManager/core/DHTService.py
+++ DHTManager/core/DHTService.py
@@ -3,7 +3,6 @@ from core.Peer import Peer
 from injector import inject
 from singleton_decorator import singleton
 
-
 @singleton
 class DHTService:
     @inject
@@ -11,17 +10,29 @@ class DHTService:
         self.__dhash_table = dht
         
     def create_peer(self, peer: Peer) -> bool:
-        print("peer created")
-        pass
+        registry = self.__dhash_table.get(peer.ip, None)
+        if not registry:
+            self.__dhash_table[peer.ip] = peer 
+            return True
+        return False
     
     def update_peer(self, peer: Peer) -> bool:
-        pass
+        registry = self.__dhash_table.get(peer.ip, None)
+        if registry:
+            self.__dhash_table[peer.ip] = peer
+            return True
+        return False
     
     def remove_peer(self, peer: Peer) -> bool:
-        pass
+        registry = self.__dhash_table.get(peer.ip, None)
+        if registry:
+            self.__dhash_table[peer.ip] = None
+            del self.__dhash_table
+            return True
+        return False
     
-    def get_peer(self) -> Peer | None:
-        pass
+    def get_peer(self, ip: str) -> Peer | None:
+        return self.__dhash_table.get(ip, None)
     
     def get_hash_table(self) -> DHT:
-        pass
+        ...

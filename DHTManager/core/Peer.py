58e34ef8c531e1fdacac120117627fa326cc479d
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-10-25 16:06:39 -0300

    _

--- DHTManager/core/Peer.py
+++ DHTManager/core/Peer.py
@@ -1,3 +1,9 @@
-class Peer():
-    def __init__(self) -> None:
-        print("created peer")
+from datetime import datetime
+
+class Peer:
+    def __init__(self, name: str, ip: str, port: int) -> None:
+        self.name = name
+        self.ip = ip
+        self.port = port
+        self.created_at = datetime.now()
+        self.last_connection_on = datetime.now()

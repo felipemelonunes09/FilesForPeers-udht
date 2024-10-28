04ceaef5a6fe6e82b90f5d58fd8878a64a6d3890
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-10-28 13:04:07 -0300

    added to_dict

--- DHTManager/core/Peer.py
+++ DHTManager/core/Peer.py
@@ -1,15 +1,24 @@
 from datetime import datetime
 from socket import socket
+from typing import Dict, Union
 
 class Peer:
-    
-    tuple = tuple[int, dict, tuple, socket]
+    ptuple = tuple[int, dict, tuple, socket]
     def __init__(self, name: str, ip: str, port: int) -> None:
         self.name = name
         self.ip = ip
         self.port = port
-        self.created_at = datetime.now()
-        self.last_connection_on = datetime.now()
+        self.created_at = str(datetime.now())
+        self.last_connection_on = str(datetime.now())
         
-    def to_tuple(self) -> tuple[str, str, str, datetime, datetime]:
+    def to_tuple(self) -> tuple[str, str, str, str, str]:
         return (self.name, self.ip, self.port, self.created_at, self.last_connection_on)
+    
+    def to_dict(self) -> Dict[str, Union[str, str]]:
+        return {
+            "name": self.name,
+            "ip": self.ip,
+            "port": self.port,
+            "createdAt": self.created_at,
+            "lastConnectionOn": self.last_connection_on
+        }
